a3e391c80aa75b4fabf0eebe4a386608d6989032
abhishek <abhishekritu810219@gmail.com>
2024-09-13 01:28:58 +0530

    first commit

--- src/main/java/dsa/linklist/practice/LL.java
+++ src/main/java/dsa/linklist/practice/LL.java
@@ -2,10 +2,46 @@ package dsa.linklist.practice;
 
 public class LL {
 
+    private Node head;
+    private Node tail;
+    private int size;
 
-    public class Node {
-        public int val;
+    public LL() {
+        this.size = 0;
+    }
+
+    private class Node {
+        int value;
         Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    // ------ implement method to insert value
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if (tail == null) {
+            tail = head;
+        }
+        size = size + 1;
+    }
+
+    public void displayValue() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + "->");
+            temp = temp.next;
+        }
+        System.out.println("end");
     }
 
 
--- src/main/java/dsa/linklist/practice/Main.java
+++ src/main/java/dsa/linklist/practice/Main.java
@@ -1,2 +1,13 @@
-package dsa.linklist.practice;public class Main {
+package dsa.linklist.practice;
+
+public class Main {
+    public static void main(String[] args) {
+        LL ll = new LL();
+        ll.insertFirst(3);
+        ll.insertFirst(4);
+        ll.insertFirst(5);
+        ll.insertFirst(6);
+        ll.displayValue();
+
+    }
 }
